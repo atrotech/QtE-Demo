95c92f0b74038b5ddedd080f326c20d5027472c8
Parsa Malekpour <malekpour.parsa@Gmail.com>
2020-12-15 18:26:45 +0330

    3

--- mainwidget.cpp
+++ mainwidget.cpp
@@ -90,24 +90,15 @@ void TMainWidget::paintEvent(QPaintEvent *)
     
     p.setPen(Qt::black);                      //  source
     p.setFont(QFont("Arial", 18));
-    p.drawText( FrameMap.width()/2 , FrameMap.height()-40 , " Source: test test ");
-    
+    p.drawText((FrameMap.width()/2)-100, FrameMap.height()-40 , " Source: test test ");
+    p.fillRect((FrameMap.width()/2)-100, FrameMap.height()-40 ,12,100,QBrush(QColor(0,0,0)));
     
     QPixmap battery("img/battery.png");  //  battery
     p.drawPixmap( width()-110 , height()/200 , 90 , 90 , battery);
     p.drawText( width()-170 , height()/200+58 , QString("%1 %").arg(ET));
    
     
-    
-    //QPixmap source("battery.png");  //  source
-    //p.drawPixmap( width()-60 , height()/100 , 90 , 90 , source);
-
-
-    
-    //p.setPen(QPen(QColor(255,255,255)));
-    //p.drawText(space,*0,width()-space*2,itemHeight,Qt::AlignLeft | Qt::AlignVCenter,QString("CPU: %1/T%2").arg(34).arg(200));
-
-
+ 
 
 
 }
