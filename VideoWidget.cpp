3302a0a7208920c3c5eee02ec0f9822baa869d7b
Parsa Malekpour <malekpour.parsa@Gmail.com>
2020-12-19 17:13:38 +0330

    de

--- VideoWidget.cpp
+++ VideoWidget.cpp
@@ -45,7 +45,7 @@ void TVideoWidget::paintEvent(QPaintEvent *)
     int HeightOffset = 60;
     
   
-    if(getGPIOValue(ButtonD_pin)==0){this->hide();QThread::msleep(2000);}
+    if(getGPIOValue(ButtonD_pin)==0){this->hide();}
     
      p.fillRect(0,0,width(),height(),QBrush(QColor(169,0,169)));
      
