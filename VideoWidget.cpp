793c5fb10bbd06d4ac4eb800c877317cdf4f7fbb
Parsa Malekpour <malekpour.parsa@Gmail.com>
2020-12-20 12:03:57 +0330

    0

--- VideoWidget.cpp
+++ VideoWidget.cpp
@@ -35,7 +35,7 @@ void TVideoWidget::paintEvent(QPaintEvent *)
 
     int SpaceHeight = 160;
     int HeightOffset = 60;
-    if(Button==4)this->hide();
+    if(Button==4){this->hide();Button=0;}
     
      p.fillRect(0,0,width(),height(),QBrush(QColor(169,0,169)));
      
