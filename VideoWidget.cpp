54d62e009dd86cb6589c9138944753cb8c3f325a
nimadorostkar <nimadorostkar97@gmail.com>
2020-12-22 16:50:20 +0330

    debug

--- VideoWidget.cpp
+++ VideoWidget.cpp
@@ -76,7 +76,7 @@ void TVideoWidget::videoTest()
   for (int i = 0; i<InputFrame.rows; i++) {
     for (int j = 0; j<InputFrame.cols; j++){
       cv::Vec3b rgb = InputFrame.at<cv::Vec3b>(i,j);
-        printf("R %d G %d B %d /n " , rgb[0], rgb[1], rgb[2]); } }
+        printf("R %d G %d B %d \n " , rgb[0], rgb[1], rgb[2]); } }
 
   QApplication::quit();
 
