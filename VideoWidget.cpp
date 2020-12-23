fc2d2c35af5062e84d22dac6dfe203f0bde4a633
nimadorostkar <nimadorostkar97@gmail.com>
2020-12-23 17:04:53 +0330

    debug and test

--- VideoWidget.cpp
+++ VideoWidget.cpp
@@ -97,9 +97,7 @@ void TVideoWidget::videoTest()
   {
      cv::Vec3b rgb = InputFrame.at<cv::Vec3b>(100,y);
      int pixColor[3] = {rgb[0]/100 , rgb[1]/100 , rgb[2]/100};
-     printf("y:%d " , y);
-     print_array(pixColor);
-     printf("\n");
+     printf("y: %d (%d %d %d) \n " , y, pixColor[0], pixColor[1], pixColor[2]);
   }
 
 
