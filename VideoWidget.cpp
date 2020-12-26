a3b01abae724369a48042b9b819350139531c7e5
nimadorostkar <nimadorostkar97@gmail.com>
2020-12-26 10:20:30 +0330

    debug

--- VideoWidget.cpp
+++ VideoWidget.cpp
@@ -81,17 +81,23 @@ void TVideoWidget::videoTest()
         cv::Vec3b rgb = InputFrame.at<cv::Vec3b>(100,y);
         int pixColor[3] = {rgb[0]/100 , rgb[1]/100 , rgb[2]/100};
 
-        if( pixColor[0] == previousPixColor[0] && pixColor[1] == previousPixColor[1] && pixColor[2] == previousPixColor[2] && (y-previousY)>10 )
+        if( pixColor[0] == previousPixColor[0] && pixColor[1] == previousPixColor[1] && pixColor[2] == previousPixColor[2] )
         {
           printf("y: %d (%d %d %d) \n " , y, pixColor[0], pixColor[1], pixColor[2]);
         }
         else
         {
           previousY = y;
-          printf("y: %d (%d %d %d)   *change color point \n " , y, pixColor[0], pixColor[1], pixColor[2]);
+          if( y-previousY > 10 )
+          {
+            printf("y: %d (%d %d %d)   *change color point \n " , y, pixColor[0], pixColor[1], pixColor[2]);
+          }
         }
         previousPixColor[0]=pixColor[0], previousPixColor[1]=pixColor[1], previousPixColor[2]=pixColor[2] ;
      }
+
+
+
 
 
 
