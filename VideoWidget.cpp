2dc75712e58ecb101dac4a0097a16312946e3ceb
nimadorostkar <nimadorostkar97@gmail.com>
2020-12-27 15:05:45 +0330

    r

--- VideoWidget.cpp
+++ VideoWidget.cpp
@@ -205,7 +205,7 @@ void TVideoWidget::videoTest()
      // Read from the text file
      ifstream MyReadFile("filename.txt");
      // Use a while loop together with the getline() function to read the file line by line
-     while (getline (MyReadFile, myText)) {
+     getline (MyReadFile, myText) {
        // Output the text from the file
        cout<<myText;
      }
