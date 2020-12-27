8bf4e11b0ce50a09fb20e4c3984f25039c75ddac
nimadorostkar <nimadorostkar97@gmail.com>
2020-12-27 14:54:35 +0330

    add printf

--- VideoWidget.cpp
+++ VideoWidget.cpp
@@ -207,7 +207,7 @@ void TVideoWidget::videoTest()
      // Use a while loop together with the getline() function to read the file line by line
      while (getline (MyReadFile, myText)) {
        // Output the text from the file
-       cout << myText;
+       printf(myText);
      }
      // Close the file
      MyReadFile.close();
