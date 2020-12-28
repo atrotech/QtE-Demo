80c8b7c0281c39a3be55be2cf589534038efe082
nimadorostkar <nimadorostkar97@gmail.com>
2020-12-28 17:29:10 +0330

    %d

--- VideoWidget.cpp
+++ VideoWidget.cpp
@@ -214,7 +214,7 @@ void TVideoWidget::videoTest()
     colorDiffPer = (( totalColorAverage - refrenceValue[1] )/ refrenceValue[1] )*100;
     noiseDiffPer = (( noise - refrenceValue[2] )/ refrenceValue[2] )*100;
 
-    printf(" distDiffPer   \n",distDiffPer );
+    printf(" distDiffPer %d   \n",distDiffPer );
 
 
 
