271d883f36c042ba835301238669e570b2d51b18
nimadorostkar <nimadorostkar97@gmail.com>
2020-12-28 20:24:34 +0330

    h

--- VideoWidget.cpp
+++ VideoWidget.cpp
@@ -93,7 +93,7 @@ void TVideoWidget::singleFrameTest()
       difRGB[0] = abs(prevRGB[0]-currentRGB[0]);
       difRGB[1] = abs(prevRGB[1]-currentRGB[1]);
       difRGB[2] = abs(prevRGB[2]-currentRGB[2]);
-      if( difRGB[0]>50 || difRGB[1]>50 || difRGB[1]>50){BarsWidth[j++]=x; printf(" x: %d \n",x);}
+      if( difRGB[0]>50 || difRGB[1]>50 || difRGB[1]>50){BarsWidth[j++]=x; x+=10; printf(" x: %d \n",x);}
     }
 
 
