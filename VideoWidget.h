ccbf7b042d2d464f6c56cf408ca866e4f70c0e79
nimadorostkar <nimadorostkar97@gmail.com>
2020-12-28 17:40:02 +0330

    float

--- VideoWidget.h
+++ VideoWidget.h
@@ -58,9 +58,9 @@ private:
 
     int refrenceValue[3];
 
-    int distDiffPer = 0;
-    int colorDiffPer = 0;
-    int noiseDiffPer = 0;
+    float distDiffPer = 0;
+    float colorDiffPer = 0;
+    float noiseDiffPer = 0;
 
     //int singleMeasured[3];
     //int videoError[3];
