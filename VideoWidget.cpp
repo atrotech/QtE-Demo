357fae2673d9a7eeb005d9fdf76f68d6fe245259
nimadorostkar <nimadorostkar97@gmail.com>
2020-12-28 18:43:49 +0330

    )

--- VideoWidget.cpp
+++ VideoWidget.cpp
@@ -209,9 +209,9 @@ void TVideoWidget::videoTest()
 
  // --------------------- difference percentage -------------------------------
 
-    distDiffPer = (( distanceAverage - refrenceValue[0] )/ refrenceValue[0]  )*100;
-    colorDiffPer = (( totalColorAverage - refrenceValue[1] )/ refrenceValue[1] )*100;
-    noiseDiffPer = (( noise - refrenceValue[2] )/ refrenceValue[2] )*100;
+    distDiffPer = ((( distanceAverage - refrenceValue[0] )/ refrenceValue[0] )*100);
+    colorDiffPer = ((( totalColorAverage - refrenceValue[1] )/ refrenceValue[1] )*100);
+    noiseDiffPer = ((( noise - refrenceValue[2] )/ refrenceValue[2] )*100);
 
 
     printf(" distanceAverage %d \n ", distanceAverage );
