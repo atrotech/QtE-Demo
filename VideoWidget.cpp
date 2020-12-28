885292c0e40a9638a562f394a6d30e61ac121bec
nimadorostkar <nimadorostkar97@gmail.com>
2020-12-28 19:50:16 +0330

    debug

--- VideoWidget.cpp
+++ VideoWidget.cpp
@@ -71,7 +71,7 @@ void TVideoWidget::paintEvent(QPaintEvent *)
 }
 
 void videoTest(){
-  
+
   singleFrameTest();
 }
 
@@ -93,7 +93,7 @@ void TVideoWidget::singleFrameTest()
         difRGB[0] = abs(prevRGB[0]-currentRGB[0]);
         difRGB[1] = abs(prevRGB[1]-currentRGB[1]);
         difRGB[2] = abs(prevRGB[2]-currentRGB[2]);
-        if( difRGB[0]>10 || difRGB[1]>10 || difRGB[1]>10 )BarsWidth[i++]=x;
+        if( difRGB[0]>10 || difRGB[1]>10 || difRGB[1]>10 )BarsWidth[x++]=x;
       }
       printf(" difRGB[0] %d \n" ,difRGB[0]);
 
