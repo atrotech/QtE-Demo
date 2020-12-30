47a93a0b5a040fe7eaf28ae485b10c24b225b696
nimadorostkar <nimadorostkar97@gmail.com>
2020-12-30 11:32:14 +0330

    -15

--- VideoWidget.cpp
+++ VideoWidget.cpp
@@ -61,22 +61,22 @@ void TVideoWidget::paintEvent(QPaintEvent *)
     p.drawText( width()/6 , height()/3+10 ,  QString(" %1 ").arg(singleMeasured[0]));
     p.drawText( width()/3-40 , height()/3+10 ,  QString(" %1 ").arg(currentValue[0]));
     p.drawText( width()/2-15 , height()/3+10 ,  QString(" %1 ").arg(refrenceValue[0]));
-    p.drawPixmap( width()/1.3+15  , height()/3-35 , tickImg);
+    p.drawPixmap( width()/1.3-15  , height()/3-35 , tickImg);
 
     p.drawText( width()/6 , height()/2-30 , QString(" %1 ").arg(singleMeasured[1]));
     p.drawText( width()/3-40 , height()/2-20 ,  QString(" %1 ").arg(currentValue[1]));
     p.drawText( width()/2-15 , height()/2-20 ,  QString(" %1 ").arg(refrenceValue[1]));
-    p.drawPixmap( width()/1.3+15   , height()/2-50 , noneImg);
+    p.drawPixmap( width()/1.3-15   , height()/2-50 , noneImg);
 
     p.drawText( width()/6 , height()/1.65 , QString(" %1 ").arg(singleMeasured[2]));
     p.drawText( width()/3-40 , height()/1.65 ,  QString(" %1 ").arg(currentValue[2]));
     p.drawText( width()/2-15 , height()/1.65 ,  QString(" %1 ").arg(refrenceValue[2]));
-    p.drawPixmap( width()/1.3  , height()/1.7-10 , cancelImg);
+    p.drawPixmap( width()/1.3-15  , height()/1.7-10 , cancelImg);
 
     p.drawText( width()/6 , height()/1.3-10 , QString(" %1 ").arg("none"));
     p.drawText( width()/3-40 , height()/1.3-10 , QString(" %1 ").arg("none"));
     p.drawText( width()/2-15 , height()/1.3-10 , QString(" %1 ").arg("none"));
-    p.drawPixmap( width()/1.3+15  , height()/1.4 , processImg);
+    p.drawPixmap( width()/1.3-15  , height()/1.4 , processImg);
 
 }
 
@@ -164,7 +164,7 @@ void TVideoWidget::singleFrameTest()
       }
     }
     currentValue[2] = colorRgb/pixQuantity ;
-    currentValue[2]= (int)(currentValue[1]*100.0)/100.0;
+    currentValue[2]= (int)(currentValue[2]*100.0)/100.0;
 
  // -------------------------- save to file -----------------------------------
     //ofstream MyFile("value.txt");
