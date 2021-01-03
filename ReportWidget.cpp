962d238f37fd7e2b0c0bf77d5d4f1a75e84472e0
nimadorostkar <nimadorostkar97@gmail.com>
2021-01-03 12:46:54 +0330

    debug again

--- ReportWidget.cpp
+++ ReportWidget.cpp
@@ -51,6 +51,6 @@ void TReportWidget::paintEvent(QPaintEvent *)
      p.setPen(Qt::black);
      p.setFont(QFont("Arial", 40));
 
-     p.drawText( width()/6 , height()/3+10 ,  QString(" report ");
+     p.drawText( width()/6 , height()/3+10 ,  QString(" report "));
 
 }
