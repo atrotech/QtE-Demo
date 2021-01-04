c54403d0cb170a356db021d5c5e6b5cdb1216c47
nimadorostkar <nimadorostkar97@gmail.com>
2021-01-04 10:26:47 +0330

    append value report

--- VideoWidget.cpp
+++ VideoWidget.cpp
@@ -209,7 +209,8 @@ void TVideoWidget::saveReportTest()
 {
 // -------------------------- save to file -----------------------------------
     ofstream MyFile("reportValue.txt");
-    MyFile << refrenceValue[0] << "," << totalMeasured[0] << "," << measuredError[0] << "\n" << refrenceValue[1] << "," << totalMeasured[1] << "," << measuredError[1] << "\n" << refrenceValue[2] << "," << totalMeasured[2] << "," << measuredError[2] << "\n" ;
+    MyFile.open ("reportValue.txt", std::ofstream::out | std::ofstream::app);
+    MyFile << refrenceValue[0] << "," << totalMeasured[0] << "," << measuredError[0] << "\n" << refrenceValue[1] << "," << totalMeasured[1] << "," << measuredError[1] << "\n" << refrenceValue[2] << "," << totalMeasured[2] << "," << measuredError[2] << std::endl ;
     MyFile.close();
 
 }
