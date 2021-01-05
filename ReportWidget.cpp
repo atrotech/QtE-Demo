1110b0f211b72a17e53a6a85027a1f126a3d469e
nimadorostkar <nimadorostkar97@gmail.com>
2021-01-05 18:33:40 +0330

    debug

--- ReportWidget.cpp
+++ ReportWidget.cpp
@@ -69,8 +69,11 @@ void TReportWidget::paintEvent(QPaintEvent *)
        }
        closedir (dir);
      }
-    for (auto file : files){}
-    cout << file << "\n";
+     else
+     {
+       perror ("opendir");
+     }
+    for (auto file : files) cout << file << "\n";
     cout << endl;
 
 
