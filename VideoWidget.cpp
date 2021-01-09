670f180b306fc2e77e514b5745c1f78ce05a237a
nimadorostkar <nimadorostkar97@gmail.com>
2021-01-09 10:55:19 +0330

    QPainter p(this)

--- VideoWidget.cpp
+++ VideoWidget.cpp
@@ -214,6 +214,11 @@ void TVideoWidget::saveReportTest()
     MyFile << refrenceValue[2] << "," << totalMeasured[2] << "," << measuredError[2] << "\n";
     MyFile << InputFrame.data;
     MyFile.close();
+
+
+    QPainter p(this);
+    p.setPen(Qt::black);
+    p.setFont(QFont("Arial", 24));
     p.drawText( width()/2-100 , height()/1.2 , QString(" %1 saved successfully  ").arg(testTime));
 
 }
