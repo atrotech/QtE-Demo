7a8d828761116df2fc745da254d7238c4aa1f7b7
nimadorostkar <nimadorostkar97@gmail.com>
2021-01-11 09:58:47 +0330

    push

--- VideoWidget.cpp
+++ VideoWidget.cpp
@@ -82,13 +82,13 @@ void TVideoWidget::paintEvent(QPaintEvent *)
     p.drawText( width()/6 , height()/1.5+10    , QString(" %1 ").arg("none"));
     p.drawText( width()/3+40 , height()/1.5+10   , QString(" %1 ").arg("none"));
     p.drawText( width()/2+60 , height()/1.5+10   , QString(" %1 ").arg("none"));
-    p.drawPixmap( width()/15  , height()/1.6  , tickImg);
+    p.drawPixmap( width()/15  , height()/1.55  , tickImg);
 // ----------------------
     p.drawText( width()/6 , height()/1.2    , QString(" %1 ").arg("none"));
     p.drawText( width()/3+40 , height()/1.2 , QString(" %1 ").arg("none"));
     p.drawText( width()/2+60 , height()/1.2 , QString(" %1 ").arg("none"));
     p.drawText( width()/1.3-100 , height()/1.2 , QString(" %1 ").arg("none"));
-    p.drawPixmap( width()/15  , height()/1.3 , tickImg);
+    p.drawPixmap( width()/15  , height()/1.25 , tickImg);
 
 
 
