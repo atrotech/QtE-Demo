e1965ac7ca9118c0706f0f511b9db68fdd721eea
nimadorostkar <nimadorostkar97@gmail.com>
2021-01-11 12:01:17 +0330

    this->paintEvent()->update()

--- VideoWidget.cpp
+++ VideoWidget.cpp
@@ -125,7 +125,8 @@ void TVideoWidget::videoTest()
   StatusIcon5 = processImg;
   StatusIcon6 = processImg;
   StatusIcon7 = processImg;
-  QPaintEvent.update();
+
+  this->paintEvent()->update();
 
 
 // --------------------------- Total Measured ---------------------------------
