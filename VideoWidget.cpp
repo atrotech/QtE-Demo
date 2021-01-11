0a8ae81f9cdfe2c16280e2a94115cc3b1da39ce4
nimadorostkar <nimadorostkar97@gmail.com>
2021-01-11 10:20:22 +0330

    push again

--- VideoWidget.cpp
+++ VideoWidget.cpp
@@ -77,7 +77,7 @@ void TVideoWidget::paintEvent(QPaintEvent *)
     p.drawText( width()/6 , height()/1.7+20    , QString(" %1 ").arg("none"));
     p.drawText( width()/3+40 , height()/1.7+20 , QString(" %1 ").arg("none"));
     p.drawText( width()/2+60 , height()/1.7+20 , QString(" %1 ").arg("none"));
-    p.drawPixmap( width()/15  , height()/1.85 , tickImg);
+    p.drawPixmap( width()/15  , height()/1.82 , tickImg);
 
     p.drawText( width()/6 , height()/1.5+10    , QString(" %1 ").arg("none"));
     p.drawText( width()/3+40 , height()/1.5+10   , QString(" %1 ").arg("none"));
@@ -87,8 +87,8 @@ void TVideoWidget::paintEvent(QPaintEvent *)
     p.drawText( width()/6 , height()/1.2    , QString(" %1 ").arg("none"));
     p.drawText( width()/3+40 , height()/1.2 , QString(" %1 ").arg("none"));
     p.drawText( width()/2+60 , height()/1.2 , QString(" %1 ").arg("none"));
-    p.drawText( width()/1.3+50 , height()/1.2 , QString(" %1 ").arg("none"));
-    p.drawPixmap( width()/15  , height()/1.3 , tickImg);
+    p.drawText( width()/1.3+10 , height()/1.2 , QString(" %1 ").arg("none"));
+    p.drawPixmap( width()/15  , height()/1.27 , tickImg);
 
 
 
