1918fa9070cf7079c2951ae17a0f99a127ccb7f3
nimadorostkar <nimadorostkar97@gmail.com>
2021-01-11 12:13:24 +0330

    true

--- VideoWidget.cpp
+++ VideoWidget.cpp
@@ -125,7 +125,7 @@ void TVideoWidget::videoTest()
   StatusIcon5 = processImg;
   StatusIcon6 = processImg;
   StatusIcon7 = processImg;
-  setUpdatesEnabled( TRUE );
+  setUpdatesEnabled(true);
   update();
 
 
