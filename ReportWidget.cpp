656c40d99e7b82c95af3eb81fad7715db504ff80
nimadorostkar <nimadorostkar97@gmail.com>
2021-01-20 09:56:26 +0330

    float

--- ReportWidget.cpp
+++ ReportWidget.cpp
@@ -68,7 +68,7 @@ void TReportWidget::ViewDataTable()
   while(MyReadFile >> ReadValue)
   {
     SelectedValue[i]=ReadValue; i++;
-    printf(" %s \n ", SelectedValue[i]);
+    printf(" %f \n ", SelectedValue[i]);
   }
   MyReadFile.close();
 
