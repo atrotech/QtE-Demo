cd3a23fdb13d184ec786ecf5837df757650934c4
Parsa Malekpour <malekpour.parsa@Gmail.com>
2021-02-03 18:11:17 +0330

    0

--- ReportWidget.h
+++ ReportWidget.h
@@ -2,13 +2,7 @@
 #include <QtWidgets>
 #include <sys/time.h>
 #include <signal.h>
-#include <opencv2/opencv.hpp>
-#include <opencv2/objdetect/objdetect.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/imgcodecs.hpp>
-#include "lib/gpio.h"
-#include "lib/common.h"
+
 #include <iostream>
 #include <fstream>
 #include <sstream>
