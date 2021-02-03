ca76a08d074b0fa9c0c022117655e8e585708078
Parsa Malekpour <malekpour.parsa@Gmail.com>
2021-02-03 20:15:42 +0330

    0

--- VideoWidget.h
+++ VideoWidget.h
@@ -3,8 +3,8 @@
 #include <sys/time.h>
 #include <signal.h>
 #include <opencv2/opencv.hpp>
-#include <opencv2/objdetect/objdetect.hpp>
-#include <opencv2/highgui/highgui.hpp>
+
+
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/imgcodecs.hpp>
 
