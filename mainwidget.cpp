a2af80a34ab751a105743e79272ca955ed6f2bc2
nimadorostkar <nimadorostkar97@gmail.com>
2021-02-06 19:03:37 +0330

    ---

--- mainwidget.cpp
+++ mainwidget.cpp
@@ -59,7 +59,7 @@ TMainWidget::TMainWidget(QWidget *parent) :
 
     setGPIOValue(LCDPowerPin,1);
     setGPIOValue(VGAPowerPin,1);
-    setGPIOValue(SwitcherPin,true);
+    setGPIOValue(SwitcherPin,1);
     setGPIOValue(ModemBPowerPin,1);
     setGPIOValue(ModemCPowerPin,1);
 
